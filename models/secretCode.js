97cd2d76d80b57e6b4981b897d219e9bc412852a
usim <shenjixia12345@naver.com>
2024-10-28 18:13:54 +0900

    Update code

--- models/secretCode.js
+++ models/secretCode.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 const secretCodeSchema = new mongoose.Schema({
     secret: { type: String, required: true },
-    value: { type: Boolean, required: false },
+    value: { type: Boolean },
     goodsname: { type: String, required: true },
     goodscode: { type: String, required: true },
-    goodsname: { type: String, required: true },
     userid: { type: String, required: true }
 });
 
