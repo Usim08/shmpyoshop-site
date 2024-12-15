dee63b259a742743e9084dc9306c963fe48f3652
usim <shenjixia12345@naver.com>
2024-12-15 18:45:20 +0900

    u

--- models/trash_data.js
+++ models/trash_data.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const ts = new mongoose.Schema({
     userName: { type: String, required: true },
-    channelId: { type: String, required: true },
-    mannagerId: { type: String, required: true }
+    channelId: { type: Number, required: true },
+    mannagerId: { type: Number, required: true }
 });
 
 const trash_data = mongoose.model('trash_data', ts, 'trash_data');
