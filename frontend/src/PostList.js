559b6dd7a1cf7d877e4a27b64e44239c3c330f67
usim <shenjixia12345@naver.com>
2025-02-24 17:21:02 +0900

    u

--- frontend/src/PostList.js
+++ frontend/src/PostList.js
@@ -1,4 +1,4 @@
-// PostList.js (수정된 부분)
+// PostList.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -7,9 +7,9 @@ const PostList = () => {
 
   useEffect(() => {
     // 게시글 목록을 불러오는 API 호출
-    axios.get('https://www.shmpyoshop.com/posts') // API 주소는 배포된 주소로 사용
+    axios.get('https://www.shmpyoshop.com/posts')
       .then(response => {
-        setPosts(response.data); // 받아온 게시글 목록을 상태에 저장
+        setPosts(response.data);
       })
       .catch(error => {
         console.error('Error fetching posts:', error);
@@ -20,13 +20,14 @@ const PostList = () => {
     <div>
       <h1>게시글 목록</h1>
       <ul>
-        {posts.map(post => (
+        {posts.length > 0 ? posts.map(post => (
           <li key={post._id}>
             <h2>{post.title}</h2>
-            {/* HTML 콘텐츠 렌더링 */}
-            <div dangerouslySetInnerHTML={{ __html: post.content }} />
+            <div
+              dangerouslySetInnerHTML={{ __html: post.content }}
+            />
           </li>
-        ))}
+        )) : <li>게시글이 없습니다.</li>}
       </ul>
     </div>
   );
