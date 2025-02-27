18768f0a1d9bee273d76c22f2c8c7d32ef961f6e
usim <shenjixia12345@naver.com>
2025-02-27 10:22:23 +0900

    u

--- server.js
+++ server.js
@@ -382,10 +382,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'home.html'));
 });
 
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'home.html'));
-});
-
 app.get('/project/service-terms', (req, res) => {
     res.sendFile(path.join(__dirname, 'project', 'service-terms.html'));
 });
